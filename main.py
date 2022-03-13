6b7f4e8d6aadecedb6fba03e792250e97d5d9759
Ammar Asmro <ammarasmaro@gmail.com>
2022-03-13 17:14:47 -0400

    Use dynamic templates to get channel ID's

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
+from functools import lru_cache
 import logging
 import os
 from pathlib import Path
 
 import discord
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+handler = logging.FileHandler(
+    filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter(
+    '%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
 intents = discord.Intents.default()
@@ -17,13 +21,40 @@ intents.members = True
 intents.guilds = True
 client = discord.Client(intents=intents)
 
-template_path = Path('./templates')
-with (template_path / 'welcome_message.txt').open(mode='r') as f:
-    welcome_template = f.read()
+
+@lru_cache
+def get_welcome_message():
+    env = Environment(
+        loader=FileSystemLoader('templates'),
+        autoescape=select_autoescape()
+    )
+    template = env.get_template('welcome_message.html')
+    return template.render(
+        rules=channels_reference['rules'].id,
+        introductions=channels_reference['introductions'].id,
+        what_are_you_reading=channels_reference['what-are-you-reading'].id,
+        tools=channels_reference['tools'].id,
+        discussion=channels_reference['discussion'].id,
+        ml_jobs=channels_reference['ml-jobs'].id,
+        memes=channels_reference['memes'].id
+    )
+
+
+def setup_channels_reference(guild):
+    # TODO: implement a class to get rid of the global variables
+    global channels_reference
+    channels_reference = {}
+    for channel in guild.channels:
+        if channel.type == discord.ChannelType.text:
+            channels_reference[channel.name] = channel
+    return channels_reference
+
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+    setup_channels_reference(client.guilds[0])
+    get_welcome_message()
 
 
 @client.event
@@ -44,6 +75,6 @@ async def on_member_join(member: discord.Member):
     guild = member.guild
     channel = guild.get_channel(channel_id=944447918524481546)
     await channel.send(f'A new member joined, info: {member}')
-    await channel.send(welcome_template)
+    await channel.send(get_welcome_message())
 
 client.run(os.environ.get('BOT_TOKEN'))
